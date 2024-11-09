3c2ea528e4f802d7b8f01e7c3697f429a3400a9e
JinYosh <ritesh.place@gmail.com>
2024-11-09 22:19:29 +0530

    New File

---
+++ LeetCode 75/Find Pivot Index.py
@@ -0,0 +1,14 @@
+def pivotIndex(nums):
+    PS = [nums[0]]
+    for i in range(1, len(nums)):
+        PS.append(PS[i-1] + nums[i])
+    for i in range(len(nums)):
+        l_sum = PS[i-1] if i-1 > -1 else 0
+        r_sum = PS[-1] - PS[i]
+        if l_sum == r_sum:
+            return i
+    return -1
+
+if __name__ == "__main__":
+    nums = [2,1,-1]
+    print("Result:",pivotIndex(nums))
